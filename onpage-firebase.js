bdc99de55ad11a4a39a1577988a865bbc716766b
Fred Jacobs <fredric100@yahoo.com>
2020-11-22 10:54:43 -0800

    Added all the code in Webstorm, and fixed errors.

--- onpage-firebase.js
+++ onpage-firebase.js
@@ -1,7 +1,9 @@
 // getting the text value from the database
 
 function run_it() {
-var bigOne = document.getElementById('bigOne');
-var dbRef = firebase.database().ref().child('text');
-dbRef.on('value', snap => bigOne.innerText = snap.val());
-}
+    alert("included javascript, run_it function");
+
+    var bigOne = document.getElementById('bigOne');
+    var dbRef = firebase.database().ref().child('text');
+    dbRef.on('value', snap => bigOne.innerText = snap.val());
+    }
