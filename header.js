c074f595533a5e67dd9010551a288afa01cd8a7d
Fred Jacobs <fredric100@yahoo.com>
2020-11-23 06:13:41 -0800

    test

--- header.js
+++ header.js
@@ -197,15 +197,7 @@ measurementId: "G-QW9SSLTXSP"
 
     alert("FINISHED initializing firebase");
 
-// getting the text value from the database
-var bigOne = document.getElementById('bigOne');
-alert("var bigOne");
 
-var dbRef = firebase.database().ref().child('text');
-alert("var dbRef");
-
-dbRef.on('value', snap => bigOne.innerText = snap.val());
-alert("snap");
 
 
 
--- onpage.js
+++ onpage.js
@@ -1,7 +1,18 @@
     alert("ONPAGE JAVASCRIPT START");
 
+    document.onload = function() {loaderFunction()};
+    function loaderFunction() {
+        alert("Document is loaded.");
+    }
+    // getting the text value from the database
+    //var bigOne = document.getElementById('bigOne');
+    //alert("var bigOne");
 
+    //var dbRef = firebase.database().ref().child('text');
+    //alert("var dbRef");
 
+    //dbRef.on('value', snap => bigOne.innerText = snap.val());
+    //alert("snap");
 
 
     alert("ONPAGE JAVASCRIPT END");
