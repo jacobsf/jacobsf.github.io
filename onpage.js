16f23d0bbdf453ab9929f01a39b88fa6080c4ce2
Fred Jacobs <fredric100@yahoo.com>
2020-11-23 09:49:42 -0800

    test

--- onpage.js
+++ onpage.js
@@ -1,21 +1,5 @@
     alert("ONPAGE JAVASCRIPT START");
 
-    <body>
-    <p>Getting started with Firebase</p>
-    <h1 id="bigOne">Wakanda Forever</h1>
-    </body>
-
-    window.onload = function() {
-        alert('Page is loaded >>> doing database read');
-
-        // getting the text value from the database
-        var bigOne = document.getElementById('bigOne');
-        var dbRef = firebase.database().ref().child('text');
-        dbRef.on('value', snap => bigOne.innerText = snap.val());
-
-        alert('FINISHED database read');
-
-    };
 
 
     alert("ONPAGE JAVASCRIPT END");
