3a5c172293743ad7160de942d6a2e34a12f77d89
Fred Jacobs <fredric100@yahoo.com>
2020-11-23 06:33:36 -0800

    test

--- onpage.js
+++ onpage.js
@@ -1,9 +1,16 @@
     alert("ONPAGE JAVASCRIPT START");
 
-    <body onLoad="loaderFunction()">
-    // document.onload = function() {loaderFunction()};
-    function loaderFunction() {
-        alert("Document is loaded.");
+    document.onload = function() {checkCookies()};
+
+        function checkCookies() {
+        var text = "";
+
+        if (navigator.cookieEnabled == true) {
+        text = "Cookies are enabled.";
+    } else {
+        text = "Cookies are not enabled.";
+    }
+        document.getElementById("demo").innerHTML = text;
     }
 
 
