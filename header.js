14b51534ed8a699c05fd7c3ac084cc6593bce46a
Fred Jacobs <fredric100@yahoo.com>
2020-11-23 02:28:20 -0800

    Added all the code in Webstorm, and fixed errors.

--- header.js
+++ header.js
@@ -1,5 +1,10 @@
 alert("HEADER JAVASCRIPT: includes Firebase inits");
 
+
+// ============================================================
+
+
+
     // Code that Fred found on StackExchange for changing images./*
 
 
@@ -103,6 +108,42 @@ alert("HEADER JAVASCRIPT: includes Firebase inits");
 });
 
 
+// ============================================================
+
+// added by Fred. A script to include HTML. Source is W3 Schools.
+
+    function includeHTML() {
+    // noinspection ES6ConvertVarToLetConst
+        var z, i, elmnt, file, xhttp;
+    /* Loop through a collection of all HTML elements: */
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+    /* Make an HTTP request using the attribute value as the file name: */
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+    if (this.readyState == 4) {
+    if (this.status == 200) {// noinspection JSReferencingMutableVariableFromClosure
+        elmnt.innerHTML = this.responseText;}
+    if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+    /* Remove the attribute, and call this function once more: */
+    elmnt.removeAttribute("w3-include-html");
+    includeHTML();
+}
+}
+    xhttp.open("GET", file, true);
+    xhttp.send();
+    /* Exit the function: */
+    return;
+}
+}
+}
+
+// ============================================================
+
 
 
 /*
@@ -146,44 +187,6 @@ $.getScript("https://www.gstatic.com/firebasejs/8.0.1/firebase-database.js", fun
 
 
 */
-
-
-
-
-// added by Fred. A script to include HTML. Source is W3 Schools.
-
-    function includeHTML() {
-    // noinspection ES6ConvertVarToLetConst
-        var z, i, elmnt, file, xhttp;
-    /* Loop through a collection of all HTML elements: */
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("w3-include-html");
-    if (file) {
-    /* Make an HTTP request using the attribute value as the file name: */
-    xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-    if (this.readyState == 4) {
-    if (this.status == 200) {// noinspection JSReferencingMutableVariableFromClosure
-        elmnt.innerHTML = this.responseText;}
-    if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-    /* Remove the attribute, and call this function once more: */
-    elmnt.removeAttribute("w3-include-html");
-    includeHTML();
-}
-}
-    xhttp.open("GET", file, true);
-    xhttp.send();
-    /* Exit the function: */
-    return;
-}
-}
-}
-
-
-
 
 
 
