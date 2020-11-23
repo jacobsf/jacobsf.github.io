6448c5325c24b23643c2206479b37239a820276e
Fred Jacobs <fredric100@yahoo.com>
2020-11-23 04:36:56 -0800

    test

--- onpage.js
+++ onpage.js
@@ -1,4 +1,12 @@
-alert("javascript edited from WebStorm");
+alert("javascript edited and included from from WebStorm");
+
+    // getting the text value from the database
+    var bigOne = document.getElementById('bigOne');
+    var dbRef = firebase.database().ref().child('text');
+    dbRef.on('value', snap => bigOne.innerText = snap.val());
+
+    alert("FINISHED getting text value from the database");
+
 
 
 
