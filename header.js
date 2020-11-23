840390fc46097a41c6f31ec64e7a53debebb566c
Fred Jacobs <fredric100@yahoo.com>
2020-11-23 05:58:53 -0800

    test

--- header.js
+++ header.js
@@ -197,7 +197,17 @@ measurementId: "G-QW9SSLTXSP"
 
     alert("FINISHED initializing firebase");
 
+// getting the text value from the database
+var bigOne = document.getElementById('bigOne');
+alert("var bigOne");
 
+bigOne.innerText = "Fred";
+alert("Fred");
+
+// var dbRef = firebase.database().ref().child('text');
+// alert("var dbRef");
+
+// dbRef.on('value', snap => bigOne.innerText = snap.val());
 
 
 
--- onpage.js
+++ onpage.js
@@ -1,16 +1,6 @@
     alert("ONPAGE JAVASCRIPT START");
 
-    // getting the text value from the database
-    var bigOne = document.getElementById('bigOne');
-    alert("var bigOne");
 
-    bigOne.innerText = 'Fred';
-    alert("Fred");
-
-        // var dbRef = firebase.database().ref().child('text');
-    // alert("var dbRef");
-
-    // dbRef.on('value', snap => bigOne.innerText = snap.val());
 
 
 
