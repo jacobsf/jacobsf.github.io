6187fa20e36405c187b0358e383c3bb96762f973
Fred Jacobs <fredric100@yahoo.com>
2020-11-23 05:27:28 -0800

    test

--- onpage.js
+++ onpage.js
@@ -1,6 +1,9 @@
     alert("ONPAGE JAVASCRIPT START");
 
-
+    // getting the text value from the database
+    var bigOne = document.getElementById('bigOne');
+    var dbRef = firebase.database().ref().child('text');
+    dbRef.on('value', snap => bigOne.innerText = snap.val());
 
     alert("ONPAGE JAVASCRIPT END");
 
