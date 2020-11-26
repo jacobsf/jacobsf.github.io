ecf557e780066e758bb178170a0fced150438584
Fred Jacobs <fredric100@yahoo.com>
2020-11-26 15:39:58 -0800

    test

--- Best-Included-Javascript-Injection40.js
+++ Best-Included-Javascript-Injection40.js
@@ -1,4 +1,4 @@
-alert("Injected Best Included Javascript40b");
+alert("Injected Best Included Javascript40c");
 
 
 alert("Waiting for page to finish loading.");
@@ -24,7 +24,9 @@ function whenLoaded() {
 
     // writing the data to the web page
     dbRef.on('value', snap => bigOne.innerText = snap.val());
-    pictures[2] = bigOne.innerText;
+    // pictures[2] = bigOne.innerText;
+    pictures[2] = "https://www.legacy.com/wp-content/uploads/2020/07/paul_newman_1600x500-1200x900.png";
+
 }
 
 
@@ -80,7 +82,6 @@ $(function () {
     }
 
     var j = 0;
-    // pictures[2] = "https://www.legacy.com/wp-content/uploads/2020/07/paul_newman_1600x500-1200x900.png";
 
 
     function shuffle(array) {
